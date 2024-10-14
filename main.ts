a978b141c3c3fa36eee6b98fddbec0ce7915156a
flowing-abyss <flowing.abyss@gmail.com>
2024-10-14 11:38:00 +0700

    Initial commit

--- main.ts
+++ main.ts
@@ -1,134 +1,263 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Plugin, PluginSettingTab, Setting } from "obsidian";
+import { join } from "path";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface TagIconPair {
+	tag: string;
+	icon: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+interface CssGeneratorSettings {
+	tagIconPairs: TagIconPair[];
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+const DEFAULT_SETTINGS: CssGeneratorSettings = {
+	tagIconPairs: [],
+};
+
+export default class CssGeneratorPlugin extends Plugin {
+	settings: CssGeneratorSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		this.addSettingTab(new CssGeneratorSettingTab(this.app, this));
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
+			id: "generate-css",
+			name: "Replace tag display (generate CSS)",
+			callback: () => this.generateCss(),
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
 	}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	async generateCss() {
+		let css = "";
+
+		// First part: Hide original tag text
+		for (const pair of this.settings.tagIconPairs) {
+			const [category, name] = pair.tag.split("/");
+			const cleanTag = `${category}${name}`;
+
+			css += `.tag[href="#${pair.tag}"],\n`;
+			css += `.cm-tag-${cleanTag},\n`;
+
+			if (name.includes("_")) {
+				css += `.cm-tag-${cleanTag} + span.cm-hashtag,\n`;
+				css += `.cm-tag-${cleanTag} + span.cm-hashtag + .cm-hashtag,\n`;
+			}
+		}
+
+		css = css.slice(0, -2); // Remove last comma and newline
+		css += ` {
+  font-size: 0px;
+  padding: 0;
+}\n\n`;
+
+		// Second part: Set font size for active tags
+		for (const pair of this.settings.tagIconPairs) {
+			const [category, name] = pair.tag.split("/");
+			const cleanTag = `${category}${name}`;
+
+			css += `.tag[href="#${pair.tag}"]:after,\n`;
+			css += `.cm-tag-${cleanTag}:after,\n`;
+
+			if (name.includes("_")) {
+				css += `.cm-tag-${cleanTag} + span.cm-hashtag:after,\n`;
+				css += `.cm-tag-${cleanTag} + span.cm-hashtag + .cm-hashtag:after,\n`;
+			}
+		}
+
+		css = css.slice(0, -2); // Remove last comma and newline
+		css += ` {
+  font-size: var(--font-text-size);
+}\n\n`;
+
+		// Third part: Set font size for active tags in editor
+		for (const pair of this.settings.tagIconPairs) {
+			const [category, name] = pair.tag.split("/");
+			const cleanTag = `${category}${name}`;
+
+			css += `.cm-active .tag[href="#${pair.tag}"],\n`;
+			css += `.cm-active .cm-tag-${cleanTag},\n`;
+
+			if (name.includes("_")) {
+				css += `.cm-active .cm-tag-${cleanTag} + span.cm-hashtag,\n`;
+				css += `.cm-active .cm-tag-${cleanTag} + span.cm-hashtag + .cm-hashtag,\n`;
+			}
+		}
+
+		css = css.slice(0, -2); // Remove last comma and newline
+		css += ` {
+  font-size: var(--font-text-size);
+}\n\n`;
+
+		// Fourth part: Set icon for each tag
+		for (const pair of this.settings.tagIconPairs) {
+			const [category, name] = pair.tag.split("/");
+			const cleanTag = `${category}${name}`;
+
+			css += `.tag[href="#${pair.tag}"]:after,\n`;
+			css += `.cm-hashtag-begin.cm-tag-${cleanTag}:after {\n`;
+			css += `  background-color: rgba(162, 93, 53, 0.1);\n`;
+			css += `  border: solid 1px rgba(209, 209, 209, 0.1);\n`;
+			css += `  border-radius: 3px;\n`;
+			css += `  content: "${pair.icon}";\n`;
+			css += `}\n\n`;
+		}
+
+		// Create the full path for the tags.css file in the .obsidian/snippets folder
+		const snippetsFolder = join(this.app.vault.configDir, "snippets");
+		const cssPath = join(snippetsFolder, "tags.css");
+
+		try {
+			// Ensure the snippets folder exists
+			await this.app.vault.adapter.mkdir(snippetsFolder);
+
+			// Write the CSS to the file
+			await this.app.vault.adapter.write(cssPath, css);
+			console.log(
+				"CSS has been written to tags.css in the snippets folder"
+			);
+		} catch (error) {
+			console.error("Error writing CSS file:", error);
+		}
+	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class CssGeneratorSettingTab extends PluginSettingTab {
+	plugin: CssGeneratorPlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: CssGeneratorPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
+		//containerEl.createEl("h2", { text: "Tag Replacer Settings" });
+
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("Tag-Icon Pairs")
+			.setDesc(
+				"Add tags and their replacement icons (e.g. task/inbox -> 📥)"
+			)
+			.addButton((button) =>
+				button.setButtonText("+").onClick(() => {
+					this.plugin.settings.tagIconPairs.push({
+						tag: "",
+						icon: "",
+					});
+					this.plugin.saveSettings();
+					this.display();
+				})
+			);
+
+		this.plugin.settings.tagIconPairs.forEach((pair, index) => {
+			const setting = new Setting(containerEl)
+				.addText((text) =>
+					text
+						.setPlaceholder("tag/tag")
+						.setValue(pair.tag)
+						.onChange(async (value) => {
+							this.plugin.settings.tagIconPairs[index].tag =
+								value;
+							await this.plugin.saveSettings();
+						})
+				)
+				.addText((text) =>
+					text
+						.setPlaceholder("icon")
+						.setValue(pair.icon)
+						.onChange(async (value) => {
+							this.plugin.settings.tagIconPairs[index].icon =
+								value;
+							await this.plugin.saveSettings();
+						})
+				)
+				.addButton((button) =>
+					button
+						.setButtonText("↑")
+						.setTooltip("Move up")
+						.onClick(async () => {
+							if (index > 0) {
+								const temp =
+									this.plugin.settings.tagIconPairs[
+										index - 1
+									];
+								this.plugin.settings.tagIconPairs[index - 1] =
+									this.plugin.settings.tagIconPairs[index];
+								this.plugin.settings.tagIconPairs[index] = temp;
+								await this.plugin.saveSettings();
+								this.display();
+							}
+						})
+				)
+				.addButton((button) =>
+					button
+						.setButtonText("↓")
+						.setTooltip("Move down")
+						.onClick(async () => {
+							if (
+								index <
+								this.plugin.settings.tagIconPairs.length - 1
+							) {
+								const temp =
+									this.plugin.settings.tagIconPairs[
+										index + 1
+									];
+								this.plugin.settings.tagIconPairs[index + 1] =
+									this.plugin.settings.tagIconPairs[index];
+								this.plugin.settings.tagIconPairs[index] = temp;
+								await this.plugin.saveSettings();
+								this.display();
+							}
+						})
+				)
+				.addButton((button) =>
+					button
+						.setButtonText("X")
+						.setTooltip("Remove")
+						.onClick(async () => {
+							this.plugin.settings.tagIconPairs.splice(index, 1);
+							await this.plugin.saveSettings();
+							this.display();
+						})
+				);
+			setting.infoEl.remove();
+		});
+
+		new Setting(containerEl).addButton((button) =>
+			button.setButtonText("+").onClick(() => {
+				this.plugin.settings.tagIconPairs.push({
+					tag: "",
+					icon: "",
+				});
+				this.plugin.saveSettings();
+				this.display();
+			})
+		);
+
+		new Setting(containerEl)
+			.setName("Generate CSS")
+			.setDesc("Generate and apply CSS based on current settings")
+			.addButton((button) =>
+				button
+					.setButtonText("Generate CSS")
+					.onClick(() => this.plugin.generateCss())
+			);
 	}
 }
